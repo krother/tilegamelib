ff9ec5367a9ebc865ea37c7b24e89b3221424acc
krother <krother@academis.eu>
2016-11-28 11:53:35 +0100

    split test_tile to 2 modules

---
+++ test/test_tile_factory.py
@@ -0,0 +1,61 @@
+
+from tilegamelib.tile_factory import TileFactory, NoTileError
+from tilegamelib.tiles import Tile
+from test_data import TILE_SPECS
+from util import TEST_GAME_CONTEXT, showdoc
+import pytest
+from pygame import Rect
+import pygame
+
+
+class TestTileFactory:
+
+    def test_get_tiles(self):
+        """Factory produces tiles."""
+        tfac = TileFactory(TILE_SPECS)
+        wall = tfac.get('b.wall')
+        assert isinstance(wall, Tile)
+
+    def test_tile_synonyms(self):
+        """Tiles can be addressed by abbreviations"""
+        tfac = TileFactory(TILE_SPECS)
+        assert isinstance(tfac.get('.'), Tile)
+        assert isinstance(tfac.get('#'), Tile)
+        assert isinstance(tfac.get('*'), Tile)
+        assert isinstance(tfac.get('x'), Tile)
+
+    def test_notile_found(self):
+        """Unknown tile raises exception"""
+        tfac = TileFactory(TILE_SPECS)
+        with pytest.raises(NoTileError):
+            tfac.get('unkown_tile')
+
+    @pytest.fixture
+    def tile_factory(self):
+        """Loads the default factory"""
+        return TileFactory(TILE_SPECS)
+
+    def test_tile_identity(self, tile_factory):
+        """Abbreviations for tiles match full name"""
+        assert tile_factory.get('b.wall') == tile_factory.get('#')
+        assert tile_factory.get('b.wall') != tile_factory.get('.')
+
+    def test_add_synonym(self, tile_factory):
+        """Synonyms can be added"""
+        tile_factory.add_tile_synonyms({'w': 'b.wall'})
+        assert tile_factory.get('w')
+
+    def test_w_not_used(self, tile_factory):
+        """adding synonym is not global"""
+        with pytest.raises(NoTileError):
+            tile_factory.get('w')
+
+    @showdoc
+    def test_display_tiles(self):
+        """Display three tiles from factory"""
+        screen = TEST_GAME_CONTEXT.screen
+        tfac = TileFactory(TILE_SPECS)
+        tfac.get('b.wall').draw(screen, Rect(32, 32, 32, 32))
+        tfac.get('x').draw(screen, Rect(32, 64, 32, 32))
+        tfac.get('*').draw(screen, Rect(64, 32, 32, 32))
+        pygame.display.update()
--- test/test_tiles.py
+++ test/test_tiles.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env python
 
-from unittest import TestCase, main
-from tilegamelib.tile_factory import TileFactory, NoTileError
 from tilegamelib.tiles import Tile
 from tilegamelib.vector import Vector
-from test_data import TILE, TILE_SIZE, TILE_SPECS
+from test_data import TILE
 from util import TEST_GAME_CONTEXT, showdoc
+import pytest
 from pygame import Rect, image
 import pygame
 
 
-class TileTests(TestCase):
+class TestTiles:
 
     @showdoc
     def test_small_tile(self):
@@ -29,32 +27,3 @@ class TileTests(TestCase):
         dest = Rect(132, 50, 32, 32)
         tile.draw(TEST_GAME_CONTEXT.screen, dest)
         pygame.display.update()
-
-
-class TileFactoryTests(TestCase):
-
-    def test_get_tiles(self):
-        """Factory produces tiles."""
-        tfac = TileFactory(TILE_SPECS)
-        red = tfac.get('b.wall')
-        self.assertTrue(isinstance(red, Tile))
-        self.assertRaises(NoTileError, tfac.get, 'limegreen')
-        # test synonyms
-        self.assertTrue(isinstance(tfac.get('.'), Tile))
-        self.assertTrue(isinstance(tfac.get('#'), Tile))
-        self.assertTrue(isinstance(tfac.get('*'), Tile))
-        self.assertTrue(isinstance(tfac.get('x'), Tile))
-
-    @showdoc
-    def test_display_tiles(self):
-        """Display three tiles from factory"""
-        screen = TEST_GAME_CONTEXT.screen
-        tfac = TileFactory(TILE_SPECS)
-        tfac.get('b.wall').draw(screen, Rect(32, 32, 32, 32))
-        tfac.get('x').draw(screen, Rect(32, 64, 32, 32))
-        tfac.get('*').draw(screen, Rect(64, 32, 32, 32))
-        pygame.display.update()
-
-
-if __name__ == "__main__":
-    main()
