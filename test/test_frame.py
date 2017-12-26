8cb48e2b4c989d6a9a27dd10866c6383d87a295f
Kristian Rother <krother@academis.eu>
2017-12-26 13:26:15 +0100

    Fixed Frame tests

--- test/test_frame.py
+++ test/test_frame.py
@@ -1,43 +1,45 @@
 
+import pytest
+
 from pygame import Rect, image
 
 from test.conftest import TILE
 from tilegamelib.config import config
 from tilegamelib.frame import Frame
-from tilegamelib.screen import Screen
-from tilegamelib.vector import Vector
-from util import graphictest, next_frame
+# from util import graphictest, next_frame
+from util import next_frame
 
 
 CYAN = (0, 255, 255)
 
 
-class FrameTests:
+@pytest.fixture
+def frame(screen):
+    return Frame(screen, Rect(50, 50, 100, 100))
 
-    def setUp(self):
-        self.screen = Screen()
-        self.frame = Frame(self.screen, Rect(50, 50, 100, 100))
 
-    @graphictest
-    def test_blit(self):
+class TestFrame:
+    """Tests for Frame objects"""
+    # @graphictest
+    def test_blit(self, frame):
         """image drawn into frame, then cleared."""
         bitmap = image.load(TILE).convert()
         dest = Rect(32, 16, 32, 32)
         source = Rect(0, 0, 32, 32)
-        self.frame.blit(bitmap, dest, source)
+        frame.blit(bitmap, dest, source)
         next_frame()
-        self.frame.clear()
+        frame.clear()
 
-    @graphictest
-    def test_text(self):
+    # @graphictest
+    def test_text(self, frame):
         """Text drawn into frame, then cleared."""
-        self.frame.print_text('Hello Frame', Vector(50, 50),
+        frame.print_text('Hello Frame', (50, 50),
             config.DEMIBOLD_BIG, config.CYAN)
         next_frame()
-        self.frame.clear()
+        frame.clear()
 
-    def test_get_dest_rect(self):
+    def test_get_dest_rect(self, frame):
         """Adds rectangle values to frame position."""
         rect = Rect(10, 20, 20, 31)
-        dest = self.frame.get_dest_rect(rect)
-        self.assertEqual(dest, Rect(60, 70, 20, 31))
+        dest = frame.get_dest_rect(rect)
+        assert dest == Rect(60, 70, 20, 31)
