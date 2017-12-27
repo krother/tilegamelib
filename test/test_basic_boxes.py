224bb238c0af0da8c3c7147636a6989ebdf0a2a2
Kristian Rother <krother@academis.eu>
2017-12-27 17:45:41 +0100

    126 tests pass

--- test/test_basic_boxes.py
+++ test/test_basic_boxes.py
@@ -1,41 +1,32 @@
 #!/usr/bin/env python
 
 import pygame
-from pygame import Rect
 
 from tilegamelib.basic_boxes import DictBox, ImageBox, TextBox
-from tilegamelib.frame import Frame
-from util import TEST_GAME_CONTEXT, showdoc
 
 
-class BasicBoxTests:
+class TestBasicBoxes:
 
-    def setUp(self):
-        self.frame = Frame(TEST_GAME_CONTEXT.screen, Rect(100, 100, 260, 160))
-
-    @showdoc
-    def test_text_box(self):
+    def test_text_box(self, frame):
         """Display hello world text box."""
-        tb = TextBox(self.frame, "Hello World")
+        tb = TextBox(frame, "Hello World")
         tb.draw()
         pygame.display.update()
 
-    @showdoc
-    def test_image_box(self):
+    def test_image_box(self, frame):
         """Display image with colorful boxes."""
-        ib = ImageBox(self.frame, "test_data/tiles.xpm")
+        ib = ImageBox(frame, "test_data/test_tile.png")
         ib.draw()
         pygame.display.update()
 
-    @showdoc
-    def test_dict_box(self):
+    def test_dict_box(self, frame):
         """Display data from a dictionary."""
         dict_data = {
             'Level': '20',
             'Name': 'Zeratul',
             'HP': '400'
         }
-        db = DictBox(self.frame, dict_data)
+        db = DictBox(frame, dict_data)
         db.draw()
         pygame.display.update()
 
@@ -43,7 +34,7 @@ class BasicBoxTests:
     # def test_life_display(self):
     #     """Display image with decreasing lives."""
     #     factory = TEST_GAME_CONTEXT.tile_factory
-    #     lifes = LifeDisplay(self.frame, factory, 10, 'g')
+    #     lifes = LifeDisplay(frame, factory, 10, 'g')
     #     for i in range(10):
     #         lifes.draw()
     #         pygame.display.update()
