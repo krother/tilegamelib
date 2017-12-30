1b8f517dff022ca019721e69aed8581972e87996
Kristian Rother <krother@academis.eu>
2017-12-30 13:21:46 +0100

    simplified code by using pygame sprite groups

--- examples/invaders.py
+++ examples/invaders.py
@@ -3,7 +3,8 @@ import random
 import time
 
 import pygame
-from pygame import K_SPACE, Rect
+from pygame import K_SPACE, Rect, mask
+from pygame.sprite import Sprite, Group, groupcollide, collide_mask
 
 from tilegamelib import AnimatedTile, TiledMap
 from tilegamelib.bar_display import BarDisplay
@@ -11,7 +12,6 @@ from tilegamelib.basic_boxes import DictBox
 from tilegamelib.config import config
 from tilegamelib.frame import Frame
 from tilegamelib.game import Game
-from tilegamelib.sprites import Sprite
 from tilegamelib.vector import DOWN, LEFT, RIGHT, UP, Vector
 
 
@@ -19,11 +19,14 @@ MIN_X = -16
 MAX_X = 764
 
 
-class Player:
+class Player(Sprite):
 
     def __init__(self, game):
+        Sprite.__init__(self)
         self.game = game
         self.tile = game.get_tile('rot.hoch')
+        self.image = self.tile.image.subsurface(self.tile.box)
+        self.g = Group(self)
         self.pos = Vector(300, 500)
         self.speed = 4
         self.direction = RIGHT
@@ -43,22 +46,25 @@ class Player:
             self.pos.x = MAX_X
 
     def draw(self):
-        self.tile.draw(self.game.frame, self.pos)
+        self.rect = Rect(self.pos.x, self.pos.y, 32, 32)
+        self.g.draw(self.game.screen.display)
 
 
-class Alien:
+class Alien(Sprite):
 
     def __init__(self, game, pos, speed=1, tile='b.ghost_d'):
+        Sprite.__init__(self)
         self.game = game
         self.tile = game.get_tile(tile)
+        self.image = self.tile.image.subsurface(self.tile.box)
         self.pos = pos
         self.speed = speed
         self.direction = RIGHT
         self.down_count = 0
         self.next_direction = None
-        self.hit = False
 
-    def move(self):
+    def update(self):
+        """Alien movement logic"""
         self.pos += self.direction * self.speed
         if self.pos.x < MIN_X:
             self.pos.x = MIN_X
@@ -74,41 +80,23 @@ class Alien:
             self.down_count -= self.speed
             if self.down_count <= 0:
                 self.direction = self.next_direction
-
-    def draw(self):
-        self.tile.draw(self.game.frame, self.pos)
-
-    def explode(self):
-        self.hit = True
-
-    @property
-    def expired(self):
-        return self.hit
+        self.rect = Rect(self.pos.x, self.pos.y, 32, 32)
 
 
-class Shot:
+class Shot(Sprite):
 
     def __init__(self, game, pos, speed=4):
+        Sprite.__init__(self)
         self.game = game
         self.tile = game.get_tile('b.dot')
+        self.image = self.tile.image.subsurface(self.tile.box)
+        self.mask = mask.from_surface(self.image)
         self.pos = Vector(pos)
         self.speed = speed
-        self.hit = False
 
-    def move(self):
-            self.pos += UP * self.speed
-
-    def draw(self):
-        self.tile.draw(self.game.frame, self.pos)
-
-    def collision(self, object):
-        return self.pos.x - 16 < object.pos.x < self.pos.x + 16 and \
-           self.pos.y - 16 < object.pos.y < self.pos.y + 16
-
-
-    @property
-    def expired(self):
-        return self.hit or self.pos.y < -20
+    def update(self):
+        self.pos += UP * self.speed
+        self.rect = Rect(self.pos.x, self.pos.y, 32, 32)
 
 
 class InvadersGame:
@@ -116,47 +104,34 @@ class InvadersGame:
     def __init__(self):
         self.game = Game()
         floorframe = Frame(self.game.screen, Rect(0, 542, MAX_X + 36, 596))
-        #self.bg = TiledMap(self.game, floorframe)
         self.floor = TiledMap(self.game, floorframe)
-        #self.bg.fill_map('#', (25, 2))
         self.floor.fill_map('#', (25, 2))
         self.player = Player(self.game)
-        self.aliens = []
-        self.shots = []
+        self.aliens = Group()
+        self.shots = Group()
         self.create_aliens()
 
     def create_aliens(self):
         for i in range(4):
             for j in range(20):
                 alien = Alien(self.game, Vector(j * 32 + 32, i * 64))
-                self.aliens.append(alien)
+                self.aliens.add(alien)
 
     def shoot(self):
         shot = self.player.get_shot()
-        self.shots.append(shot)
-
-    def check_collisions(self):
-        for s in self.shots:
-            for a in self.aliens:
-                if s.collision(a):
-                    a.explode()
-                    s.hit = True
+        self.shots.add(shot)
 
     def draw(self):
         self.game.screen.clear()
         self.floor.draw()
         self.player.move()
         self.player.draw()
-        for s in self.shots:
-            s.move()
-            s.draw()
-        for a in self.aliens:
-            a.move()
-            a.draw()
-        self.check_collisions()
-        self.aliens = [a for a in self.aliens if not a.expired]
-        self.shots = [s for s in self.shots if not s.expired]
-        if len(self.aliens) == 0:
+        self.shots.update()
+        self.shots.draw(self.game.screen.display)
+        self.aliens.update()
+        self.aliens.draw(self.game.screen.display)
+        groupcollide(self.shots, self.aliens, True, True, collided=collide_mask)
+        if not self.aliens:
             self.game.exit()
 
     def run(self):
